7b47dff0bcf1b6b20810efafe210fbe752ca0e37
lvulpes <l.vulpes@proton.me>
2025-09-19 14:14:09 +0200

    Added wrapper script to manage virtual env

---
+++ adsb_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Load pyenv environment
+export PYENV_ROOT="$HOME/pyenv"
+export PYENV_ADSB="$PYENV_ROOT/adsb_rx/bin/activate"
+export PATH="$PYENV_ROOT/adsb_rx/bin:$PATH"
+export PY_PATH="$HOME/apps/adsb_rx/"
+export PY_SRC="get_adsb_data.py"
+eval "$(source $PYENV_ADSB)"
+
+# Navigate to your project directory
+cd $PY_PATH
+
+# Run your Python script with the pyenv-managed Python
+python $PY_SRC
+
+# Optional: Deactivate the environment (good practice for clean scripts)
+deactivate
